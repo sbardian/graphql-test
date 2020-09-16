b0dcf598dc8d4f036e5bc9def3b4538bf44854ce
Brian Andrews <sbardian@gmail.com>
2020-09-16 00:07:24 -0600

    feat: implement sorting and filtering todos

--- src/graphql/resolvers/queries/get-todos.js
+++ src/graphql/resolvers/queries/get-todos.js
@@ -1,8 +1,43 @@
+/* eslint-disable array-callback-return, consistent-return */
+
+const getOrderByInputField = (orderByInput) => {
+  const orderByInputFields = Object.keys(orderByInput)
+  if (orderByInputFields.length !== 1) {
+    throw new Error(
+      `orderBy input list items may only contain 1 field (received: ${orderByInputFields})`,
+    )
+  }
+  return orderByInputFields[0]
+}
+
+const createOrderBy = (orderBy = []) => {
+  const customOrderBy = orderBy.map((orderByInput) => {
+    const field = getOrderByInputField(orderByInput)
+    switch (field) {
+      case 'priority':
+        return ['priority', orderByInput[field]]
+      case 'description':
+        return ['description', orderByInput[field]]
+      case 'created_at':
+        return ['created_at', orderByInput[field]]
+      default:
+        break
+    }
+  })
+  return customOrderBy.length > 0 ? customOrderBy : []
+}
+
 export const getTodos = async (
   root,
-  _,
+  { orderBy = [], completed = null },
   { loaders: { getTodosLoader }, models: { Todo } },
 ) => {
-  const todos = await Todo.find({})
+  const sort = createOrderBy(orderBy)
+  let todos
+  if (completed === null) {
+    todos = await Todo.find({}).sort(sort)
+  } else {
+    todos = await Todo.find({ completed: { $eq: completed } }).sort(sort)
+  }
   return todos.map((todo) => getTodosLoader.load(todo.id))
 }
