2dab193ef7084e736ee0d79cb4dd4764e688fe18
Brian Andrews <sbardian@gmail.com>
2020-09-15 21:25:03 -0600

    refactor: allow using port env var

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import { createServer } from './server'
 
 const server = async () => {
   const { httpServer } = await createServer()
-  httpServer.listen({ port: '9999' })
+  httpServer.listen({ port: process.env.port || 9999 })
 }
 
 server()
